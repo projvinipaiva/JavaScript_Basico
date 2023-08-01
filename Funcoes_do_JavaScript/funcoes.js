313f4d037eecaad411d576286e337bc9d168f27e
♥ Vinícius de Paiva Mendonça Silva ♥ <100208580+projvinipaiva@users.noreply.github.com>
2023-08-01 12:42:46 -0300

    Add files via upload

---
+++ Funcoes_do_JavaScript/funcoes.js
@@ -0,0 +1,9 @@
+saldo=1000
+function dadosacesso(){
+    numcartão = prompt("digite io numero do cartão")
+    senha = prompt("Digite a senha:")
+}
+function saque(valor){
+    saldo = saldo - valor
+    return saldo
+}
