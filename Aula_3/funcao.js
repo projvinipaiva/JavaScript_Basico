20db65dd9ccdbff62b8087cbb89e393ff44d3292
♥ Vinícius de Paiva Mendonça Silva ♥ <100208580+projvinipaiva@users.noreply.github.com>
2023-09-11 19:39:19 -0300

    Add files via upload

--- Aula_3/funcao.js
+++ Aula_3/funcao.js
@@ -3,16 +3,26 @@ var email
 var num = 0;
 saldo = 2000;
 var saque = 0;
+var deposito = 0
 
 function sacar(saque){
     saldo = saldo - saque
     return saldo
 }
 
+function depositar(deposito){
+    saldo = saldo + deposito
+    return saldo
+ }
+
 function listar(quadro){
+    info = "Seja bem vindo: ";
     info = "Usuário número " + (++num) + "\n";
     info += "Nome: " + nome + "\n";
     info += "Email: " + email + "\n";
-    info += "Saldo: " + sacar(saque) + "\n \n"
+    info += "Saldo inicial: " + saldo + "\n";
+    info += "Saldo após sacar:" + sacar(saque) + "\n";
+    info += "Saldo após depositar:" + depositar(deposito) + "\n \n";
+    
     quadro.value += info;
 }
