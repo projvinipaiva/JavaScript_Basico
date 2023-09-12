5da8832655514369c1065ef83cfc386eb9a076e2
♥ Vinícius de Paiva Mendonça Silva ♥ <100208580+projvinipaiva@users.noreply.github.com>
2023-09-12 10:56:26 -0300

    Add files via upload

--- Aula_4/imc2.js
+++ Aula_4/imc2.js
@@ -2,24 +2,25 @@ function calcula_imc(event) {
 
     event.preventDefault(); // Evita o envio do formulário
 
-    var altura = document.imcForm.altura.value;
-    var peso = document.imcForm.peso.value;
-
-    var calculo = peso / (altura * altura);
-
-    var resultado;
-
-    if (calculo < 18.5) {
-        resultado = "Você está magro com esse índice: " + calculo;
-    } else if (calculo >= 18.5 && calculo < 24.9) {
-        resultado = "Você está normal com esse índice: " + calculo;
-    } else if (calculo >= 25 && calculo < 29.9) {
-        resultado = "Você está com sobrepeso com esse índice: " + calculo;
-    } else if (calculo >= 30 && calculo < 39.9) {
-        resultado = "Você está com obesidade com esse índice: " + calculo;
-    } else if (calculo >= 40) {
-        resultado = "Você está com obesidade grave com esse índice: " + calculo;
+        var altura = document.imcForm.altura.value;
+        var peso = document.imcForm.peso.value;
+    
+        var calculo = (peso/(altura*altura));
+    
+        if(calculo<18.5){
+            alert("Você está magro com esse indice " + calculo);
+        }
+        else if (calculo>=18.5 && calculo<24.9){
+            alert("Você está normal com esse indice " + calculo);
+        }
+        else if (calculo>=25 && calculo<29.9){
+            alert("Você está com sobre peso com esse indice " + calculo);
+        }
+        else if (calculo>30 && calculo<39.9){
+            alert("Você está com obesidade com esse indice " + calculo);
+        }
+        else if (calculo>40){
+            alert("Você está com obesidade grave com esse indice " + calculo);
+        }
+    
     }
-
-    document.getElementById('resul').innerHTML = resultado;
-}
